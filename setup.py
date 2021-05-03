ec03931baf002a35151c3af025577e51fd782c51
richard <ruanchuanwei@gmail.com>
2021-05-02 17:09:46 -0700

    Add requirement;

--- setup.py
+++ setup.py
@@ -3,5 +3,13 @@ from setuptools import setup, find_packages
 print(find_packages())
 setup(name='deeprecsys',
       version='0.01',
+      install_requires=[
+            'pandas>=1.2.1',
+            'torch>=1.7.0',
+            'tqdm',
+            'numpy',
+            'scipy',
+            'scikit_learn'
+      ],
       packages=['deeprecsys'],
       package_data={"deeprecsys": ["py.typed"]})
