d65a4bc34d83eca4c6f9415c84736db6141d8931
richard <ruanchuanwei@gmail.com>
2021-05-22 22:33:04 -0700

    update eval;

--- deeprecsys/eval.py
+++ deeprecsys/eval.py
@@ -1,14 +1,19 @@
 import logging
-from typing import List, Optional, Dict, Set, Iterable
+from typing import List, Optional, Dict, Set, Iterable, Sequence
 
 import pandas as pd
 import torch
 from numpy.random import RandomState
-from scipy import sparse as sp #type: ignore
-import numpy as np #type: ignore
-from sklearn.utils.extmath import randomized_svd #type: ignore
-from tqdm import tqdm #type: ignore
+from scipy import sparse as sp  # type: ignore
+import numpy as np  # type: ignore
+from sklearn.utils.extmath import randomized_svd  # type: ignore
+from tqdm import tqdm  # type: ignore
 from deeprecsys.base_recommender import Recommender
+
+
+def sigmoid(x: np.ndarray, min_: float = -8, max_: float = 8) -> np.ndarray:
+    x = np.minimum(max_, np.maximum(min_, x))
+    return np.exp(x) / (1 + np.exp(x))
 
 
 def unbiased_eval(num_user: int, num_item: int, dat_df: pd.DataFrame,
@@ -21,7 +26,7 @@ def unbiased_eval(num_user: int, num_item: int, dat_df: pd.DataFrame,
     prng = RandomState(seed)
 
     row, col = dat_df.uidx, dat_df.iidx
-    def sigmoid(x): return np.exp(x) / (1 + np.exp(x))
+
     recall_cnt = 0
     ndcg_sum = 0
     for u, i in list(zip(row, col)):
@@ -32,11 +37,9 @@ def unbiased_eval(num_user: int, num_item: int, dat_df: pd.DataFrame,
             neg = prng.randint(0, num_item, num_neg)
             for idx in range(num_neg):
                 if int(neg[idx]) in past_hist.get(u, []) or i == neg[idx]:
-                    while int(
-                            neg[idx]) not in past_hist.get(
-                            u, []) and i != neg[idx]:
+                    while int(neg[idx]) not in past_hist.get(u, []) and i != neg[idx]:
                         neg[idx] = prng.randint(0, num_item)
-        item_list: List[int] = neg.tolist()
+        item_list: List[int] = neg.tolist()  # type: ignore
         item_list.append(i)
         user_list = [u] * len(item_list)
         scores = recom.score(user_list, item_list)
@@ -54,28 +57,38 @@ def unbiased_eval(num_user: int, num_item: int, dat_df: pd.DataFrame,
         item_npy = np.array(item_list)
         top_items = item_npy[rank][:cut_len]
         top_item_rel_prob = rel_prob[rank][:cut_len]
-        #recall_cnt += int(i in top_items)
+        # recall_cnt += int(i in top_items)
         for pos, (top_i, top_rel) in enumerate(
                 zip(top_items, top_item_rel_prob)):
             if i == top_i:
                 recall_cnt += (top_rel / expo_score)
                 ndcg_sum += np.log(2) / np.log(2 + pos) * \
-                    (top_rel / expo_score)
+                            (top_rel / expo_score)
     logger.info(
         f'Recall@{cut_len} = {recall_cnt / len(row):.5f}; NDCG@{cut_len} = {ndcg_sum / len(row):.5f}')
     return {'recall': recall_cnt / len(row), 'ndcg': ndcg_sum / len(row)}
 
 
-def unbiased_full_eval(user_num: int, item_num: int, dat_df: pd.DataFrame, recom:Recommender, topk: int,
+def unbiased_full_eval(user_num: int, item_num: int,
+                       recom: Recommender, topk: int,
+                       dat_df: Optional[pd.DataFrame] = None,
                        rel_model: Optional[Recommender] = None):
-    # generate recommendations for for all items
     logger = logging.getLogger(__name__)
     candidate_items = list(range(item_num))
-    user_top_items: Dict[int, Iterable[int]] = {}
-    for user in dat_df.uidx.unique():
+    user_top_items: Dict[int, Sequence[int]] = {}
+
+    if not dat_df and not rel_model:
+        raise ValueError('Need to input dat_df or rel_model')
+
+    if dat_df:
+        users = dat_df.uidx.unique().tolist()
+    else:
+        users = list(range(user_num))
+
+    for user in users:
         _users = [user] * len(candidate_items)
         scores = recom.score(_users, candidate_items)
-        sorted_idx = np.argsort(scores)[-topk:]
+        sorted_idx = np.argsort(scores)[-topk:][::-1]
         user_top_items[user] = sorted_idx
 
     cum_user_score = 0
@@ -83,13 +96,13 @@ def unbiased_full_eval(user_num: int, item_num: int, dat_df: pd.DataFrame, recom
         for user, top_items in user_top_items.items():
             _users = [user] * len(top_items)
             rel_score = rel_model.score(_users, list(top_items))
-            rel_score = np.minimum(8, np.maximum(-8, rel_score))
-            rel_score = np.exp(rel_score) / (1 + np.exp(rel_score))
+            rel_score = sigmoid(rel_score)
             cum_user_score += rel_score.mean()
     else:
         user_label = {(uidx, iidx): rating for uidx, iidx, rating in zip(dat_df.uidx,
                                                                          dat_df.iidx,
                                                                          dat_df.rating)}
+
         def empirical_score(user, items, user_label):
             score = []
             for item in items:
@@ -98,12 +111,10 @@ def unbiased_full_eval(user_num: int, item_num: int, dat_df: pd.DataFrame, recom
 
         for user, top_items in user_top_items.items():
             cum_user_score += empirical_score(user, top_items, user_label)
-    rel_score = cum_user_score / len(user_top_items)
+    rel_score = cum_user_score / len(users)
     if rel_model:
         logger.info(f'Full relevance score: {rel_score}')
     else:
         logger.info(f'Full empirical relevance score: {rel_score}')
 
     return rel_score
-
-
