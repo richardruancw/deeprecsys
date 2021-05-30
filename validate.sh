e5f6f49e2a2351d5036c4a9591ff77c37777aed9
richard <ruanchuanwei@gmail.com>
2021-05-30 03:24:44 -0700

    1. Allow g and w to be different from f. 2. Allow g and w to use embeddings from f.

--- validate.sh
+++ validate.sh
@@ -1,20 +1,17 @@
 # Test attention implementation, this setting == train attention model directly
-run_reweight_v2.py --model seq --cuda_idx 0 --lambda_ 0 --w_lower_bound 1.0 --g_step 0 --w_step 0
+python run_reweight_v2.py --model seq --cuda_idx 0 --lambda_ 0 --w_lower_bound 1.0 --g_step 0 --w_step 0
 #expect relevance score
 #INFO:deeprecsys.eval:Full empirical relevance score: 0.015552980132450437
 
-
-# Test the reweight training using attention
-run_reweight_v2.py --model seq --cuda_idx 0 --lambda_ 1 --w_lower_bound 1.0 --g_step 1 --w_step 1
-#INFO:root:biased eval for SVD model on test
-#INFO:deeprecsys.eval:Full empirical relevance score: 0.01585596026490078
-#INFO:root:------Reweight and rebalance model ------
-#DEBUG:deeprecsys.data:Build windowed data
-#DEBUG:deeprecsys.data:Build windowed data
-#DEBUG:deeprecsys.data:Build windowed data
-#4825
-#INFO:deeprecsys.eval:Full empirical relevance score: 0.015360927152317965
-#4825
-#INFO:deeprecsys.eval:Full empirical relevance score: 0.01661589403973525
-#4825
+# Test the reweight training using attention with bound
+python run_reweight_v2.py --model seq --cuda_idx 0 --lambda_ 1 --w_lower_bound 1.0 --g_step 1 --w_step 1
 #INFO:deeprecsys.eval:Full empirical relevance score: 0.016677152317880944
+
+# Use different baseclass for w and g
+python run_reweight_v2.py --model seq --cuda_idx 0 --lambda_ 1 --w_lower_bound 0.1 --g_step 1 --w_step 1 --w_model mlp --g_model mlp
+
+# Use attention model for g and w shared item embedding from f
+python run_reweight_v2.py --model seq --cuda_idx 0 --lambda_ 1 --w_lower_bound 0.1 --g_step 1 --w_step 1 --share_f_embed
+
+# Use mf model for g and w shared item embedding from f
+python run_reweight_v2.py --model mf --cuda_idx 0 --lambda_ 1 --w_lower_bound 0.1 --g_step 1 --w_step 1 --share_f_embed
